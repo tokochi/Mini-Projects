4c733942239fd54d8e19ad4c8638b64ec14ada31
tokochi <tokochi@hotmail.fr>
2021-03-21 18:06:36 +0100

    joke teller done

---
+++ 5.Picture-in-Picture/script.js
@@ -0,0 +1,24 @@
+const videoElement = document.querySelector("#video") 
+const button = document.querySelector("#button") 
+
+
+async function selectMediaStream(){
+    try {
+        const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+        videoElement.srcObject = mediaStream;
+        videoElement.onloadedmetadata = () =>{
+            videoElement.play();
+        }
+    } catch (error) {
+        
+    }
+
+}
+
+button.addEventListener('click', async () => {
+    button.disabled = true;
+    await videoElement.requestPictureInPicture();
+    button.disabled = false;
+} )
+
+selectMediaStream()
